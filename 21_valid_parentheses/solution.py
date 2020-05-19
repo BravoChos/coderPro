001de0a9f1f1b0284163c03edfd89f6dba904a61
Song Cho <deeplearner507@gmail.com>
2020-05-19 22:28:53 +0900

    21_valid_parenthses

---
+++ 21_valid_parentheses/solution.py
@@ -0,0 +1,35 @@
+class Solution:
+  def isValid(self, s):
+    stack = []
+    for c in s:
+      if c == '(':
+        stack.append('(')
+      if c == ')':
+        if len(stack) == 0:
+          return False
+        if stack[-1] != '(':
+          return False
+        else:
+          stack.pop()
+      if c == '{':
+        stack.append('{')
+      if c == '}':
+        if len(stack) == 0:
+          return False
+        if stack[-1] != '{':
+          return False
+        else:
+          stack.pop()
+      if c == '[':
+        stack.append(']')
+      if c == ']':
+        if len(stack) == 0:
+          return False
+        if stack[-1] != ']':
+          return False
+        else:
+          stack.pop()
+    if len(stack) > 0:
+      return False
+    else:
+      return True
